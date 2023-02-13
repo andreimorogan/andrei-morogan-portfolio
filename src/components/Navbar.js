7b9aa2caee48fc04a74c4b7bbcaea7c5bfa6b4b0
andreymorogan <andreymorogan@gmail.com>
2023-02-13 21:27:18 +0200

    Converted hamburger toggle from vanilla js to React

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,30 @@
 import React from "react";
 import Logo from '../assets/logo.svg';
 import './Navbar.css';
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const [btnState, setBtnState] = useState(false);
+
+    const handleClick = () => {
+        setBtnState(btnState => !btnState);
+    }
+
+    let toggleClassCheck = btnState ? ' active' : null;
+
     return (
         <>
             <nav>
                 <img src={Logo} alt="The logo." className="logo-img" />
                 <div className="group">
-                    <ul className="navigation">
+                    <ul className= {`navigation ${toggleClassCheck}`}>
                         <li><a href="index.html">About</a></li>
                         <li><a href="projects.html">Projects</a></li>
                         <li><a href="contact.html">Contact</a></li>
                     </ul>
                 </div>
-                <div className="hamburger">
+                <div className={`hamburger ${toggleClassCheck}`} onClick={handleClick}>
                     <span className="bar"></span>
                     <span className="bar"></span>
                     <span className="bar"></span>
