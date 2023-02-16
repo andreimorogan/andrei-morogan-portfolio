4dc27d02e92511e46273a24f6e188f6368689c87
andreymorogan <andreymorogan@gmail.com>
2023-02-16 22:05:20 +0200

    Added reveal on scroll animations, fade in from top animation for the navbar and added an About Me section

---
+++ src/components/Lorem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Lorem.css'
+
+const Lorem = () => {
+    return (
+        <p className='main-greeting '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptatum dolores eum quae corrupti enim exercitationem alias tempore. Optio repudiandae ipsa ea aliquid nulla quod recusandae exercitationem quis facilis? Nostrum?</p>
+    );
+}
+
+export default Lorem
