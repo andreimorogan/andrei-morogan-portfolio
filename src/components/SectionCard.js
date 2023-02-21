52ca860ecb7208d18ad9c245567159a3f98cf27b
andreymorogan <andreymorogan@gmail.com>
2023-02-21 15:40:20 +0200

    Added an image of myself

--- src/components/SectionCard.js
+++ src/components/SectionCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from './SectionCard.module.css'
+import me from '../assets/me.jpg'
 
 
 const SectionCard = () => {
@@ -9,9 +10,11 @@ const SectionCard = () => {
             <hr></hr>
             <div className={`${styles.gray} ${styles.small}`}>
                 <p>I enjoy building websites, solving issues and creating applications that tailor towards the need of the user.</p>
+                <p>Currently I'm working at Mega Image SRL, as a Data Quality Controller.</p>
                 <p>My end goal is becoming a full-stack developer.</p>
                 <p>In my free time I enjoy learning new languages, tinkering with technology and occasionally hiking or just taking a plain old walk.</p>
                 <p>My favorite color is <span className={styles.purple}>purple</span>.</p>
+                <img className={styles.me} src={me} alt="Myself"/>
             </div>
         </section>
     );
