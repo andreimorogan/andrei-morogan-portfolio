0f964a09b3431cd91df15ce6b88ff7f971c8e7c3
andreymorogan <andreymorogan@gmail.com>
2023-03-14 16:29:45 +0200

    feat: add form json req functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,56 @@
-import styles from "../styles/Form.module.css"
-import common from "../styles/Common.module.css"
+import React, { useState } from 'react';
+import styles from '../styles/Form.module.css';
+import common from '../styles/Common.module.css';
 
 const Form = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+
+    const onNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const onEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const onMessageChange = (e) => {
+        setMessage(e.target.value);
+    }
+
+
+    const onMessageSubmit = () => {
+        fetch('http://localhost:3001/submit', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                message: message
+            })
+        })
+        .catch((err) => console.log(err));
+        document.getElementById('contact-form').reset();
+    }
+
+
     return (
         <div>
-            <form className={`${styles.container} ${common.aligned} ${common.small}`}>
+            <form id='contact-form' className={`${styles.container} ${common.aligned} ${common.small}`}>
                 <label htmlFor="name" >Your name<span className={common.purple}>*</span></label>
-                <input id="name" type="text" placeholder="Whichever name you wish." required={true}></input>
+                <input id="name" type="text" placeholder="Whichever name you wish." required={true} onChange={onNameChange}></input>
                 <label htmlFor="email">Your email address<span className={common.purple}>*</span></label>  
-                <input id="email" type="email" placeholder="example@domain.com"></input>
+                <input id="email" type="email" placeholder="example@domain.com" onChange={onEmailChange}></input>
                 <label htmlFor="message">Your message for me<span className={common.purple}>*</span></label>
-                <textarea type="textarea" id="message" placeholder="Say hello!" rows="10" cols="50"></textarea>
-                <input type='submit' value="Submit"/>
+                <textarea type="textarea" id="message" placeholder="Say hello!" rows="10" cols="50" onChange={onMessageChange}></textarea>
+                <input type='submit' value="Submit" onClick={onMessageSubmit} />
             </form>
         </div>
     );
 }
 
-export default Form
+
+export default Form;
