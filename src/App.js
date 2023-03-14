6f0afe831ee465bba543bd5c4490090c6c1e0044
andreymorogan <andreymorogan@gmail.com>
2023-03-14 21:28:16 +0200

    fix: add scroll to top when switching routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import { Routes, Route, HashRouter} from 'react-router-dom';
 import Contact from './pages/Contact';
+import ScrollToTop from "./components/ScrollToTop";  
 
 function App() {
   return (
     <React.StrictMode>
       <HashRouter>
+      <ScrollToTop />
           <Routes>
             <Route index element={<About />} />
             <Route path='contact' element={<Contact />} />
